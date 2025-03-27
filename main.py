70b7f61c1f00d64596e5ac91d53aa8715737bd14
Ilya Chernooki <ilac10765@gmail.com>
2025-03-27 20:23:28 +0300

    Bot 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-hello
+import asyncio
+from aiogram import Bot,Dispatcher,types
+
+
+TOKEN = "7858050871:AAHQDdZTj8N0BvOfpds0ee3k_S1yMmxxknA"
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("в работе")
+async def main():
+    print("Bot start")
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
+
