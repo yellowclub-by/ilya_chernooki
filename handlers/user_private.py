0cf892e3edd9ba0d61e1f72186aa02eeb63b3c8e
Ilya Chernooki <ilac10765@gmail.com>
2025-04-15 20:30:56 +0300

    lesson3

---
+++ handlers/user_private.py
@@ -0,0 +1,29 @@
+from aiogram.filters import CommandStart, Command
+from aiogram import types,Router,F
+
+user_router = Router()
+
+@user_router.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("Привет это бот для продажи В_баксов")
+
+@user_router.message(F.text.lower().contain("каталог"))
+@user_router.message(Command("catalog"))
+async def catalog(message: types.Message):
+    await message.answer("Добро пожаловать в каталог")
+
+@user_router.message(Command("donat"))
+async def donat(message: types.Message):
+    await message.answer("Спасибо за донат")
+
+@user_router.message(Command("support"))
+async def support(message: types.Message):
+    await message.answer("Это раздел  поддержки")
+
+@user_router.message(Command("basket"))
+async def basket(message: types.Message):
+    await message.answer("Здесь хранятся ваши выбранные товары")
+
+@user_router.message(F.text.lower().contains("стои")|F.text.lower().contains("цен"))
+async def echo(message: types.Message):
+    await message.answer("в работе")
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import asyncio
-from aiogram import Bot,Dispatcher,types
+from aiogram import Bot, Dispatcher
 
 
 TOKEN = "7858050871:AAHQDdZTj8N0BvOfpds0ee3k_S1yMmxxknA"
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+from handlers.user_private import user_router
+dp.include_router(user_router)
 
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer("в работе")
 async def main():
     print("Bot start")
     await dp.start_polling(bot)
 
 
 asyncio.run(main())
-
