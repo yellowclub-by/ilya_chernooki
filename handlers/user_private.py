f0d13031a72a402d8fec024fafd0601f91eda0b0
Ilya Chernooki <ilac10765@gmail.com>
2025-04-29 20:35:33 +0300

    кнопачки

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,16 +1,17 @@
 from aiogram.filters import CommandStart, Command
 from aiogram import types,Router,F
+from Keyboards import reply
 
 user_router = Router()
 
 @user_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer("Привет это бот для продажи В_баксов")
+    await message.answer("Привет это бот для продажи В_баксов", reply_markup=reply.main_kb)
 
 @user_router.message(F.text.lower().contain("каталог"))
 @user_router.message(Command("catalog"))
 async def catalog(message: types.Message):
-    await message.answer("Добро пожаловать в каталог")
+    await message.answer("Добро пожаловать в каталог", reply_markup=reply.catalog_kb)
 
 @user_router.message(Command("donat"))
 async def donat(message: types.Message):
